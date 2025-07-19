ec9522085ac5b575adcc9ce34515d64d7f0f42d4
MUKESH KUMAR SONI <mukeshkumarsoni990@gmail.com>
2025-07-19 15:25:57 +0530

    Add View on GitHub button to each project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -46,6 +46,14 @@ const Projects = () => {
               >
                 View on GitHub
               </a>
+              <a
+                href={liveLinks[repo.name]}
+                target="_blank"
+                rel="noreferrer"
+                className="inline-block px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
+              >
+                 Go Live
+              </a>
             </motion.div>
           ))}
         </div>
