e9c5d0a699e934dc3d771f9899e5b9acf4a6b1ef
MUKESH KUMAR SONI <mukeshkumarsoni990@gmail.com>
2025-07-28 12:47:05 +0530

    Added new website

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,8 @@ const liveLinks = {
   "portfolio-website": "https://portfolio-website-inky-eight-27.vercel.app/",
   "Molecule-Housing-Market": "https://molecule-housing-market.web.app/",
   "Simple-interest-calculator": "https://simple-interest-calculator-tan.vercel.app/",
-  "Virtual-Assistant-by-using-Python": "https://virtual-assistant-by-using-python.onrender.com/"
+  "Virtual-Assistant-by-using-Python": "https://virtual-assistant-by-using-python.onrender.com/",
+  "BMI-Calculator": "https://ai-bmi-calculator.netlify.app/"
   // Add more repo names and links here
 };
 
